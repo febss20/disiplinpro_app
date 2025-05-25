021d562cf0632014ef4bf53d34800f4f416f422e
febss.rasyid <alifrasyidf@gmail.com>
2025-05-25 08:06:50 +0700

    fix(navigation): repair and enhance app navigation flow

---
+++ app/src/main/java/com/dsp/disiplinpro/ui/theme/Spring.kt
@@ -0,0 +1,20 @@
+package com.dsp.disiplinpro.ui.theme
+
+import androidx.compose.animation.core.Spring
+
+/**
+ * Konstanta untuk animasi Spring
+ */
+object Spring {
+    // Dampening Ratio
+    const val DampingRatioHighBouncy = 0.2f
+    const val DampingRatioMediumBouncy = 0.5f
+    const val DampingRatioLowBouncy = 0.75f
+    const val DampingRatioNoBouncy = 1f
+
+    // Stiffness
+    const val StiffnessHigh = 1000f
+    const val StiffnessMedium = 400f
+    const val StiffnessLow = 200f
+    const val StiffnessVeryLow = 50f
+}
